ee72238de5b2b33307af3a53460aa3751c1e9d80
Junaid Hussain <47213023+junnhssn@users.noreply.github.com>
2023-06-02 19:08:07 +0530

    Update configurevm.ps1

--- Allfiles/Labs/10/configurevm.ps1
+++ Allfiles/Labs/10/configurevm.ps1
@@ -1,10 +1,29 @@
 # Install Chocolatey Packager
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+Function InstallChocolatey
+{   
+    $env:chocolateyUseWindowsCompression = 'true'
+    $env:chocolateyIgnoreRebootDetected = 'true'
+    $env:chocolateyVersion = '1.4.0'
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+    choco feature enable -n allowGlobalConfirmation
+}
+InstallChocolatey
 
 # Silent Install Software Tools
 # Install System Tools
 # Install Microsoft Edge
-choco install microsoft-edge -confirm:$false
+Function InstallEdgeChromium
+{
+    #Download and Install edge
+    $WebClient = New-Object System.Net.WebClient
+    $WebClient.DownloadFile("http://go.microsoft.com/fwlink/?LinkID=2093437","C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi")
+    sleep 5
+    
+    Start-Process msiexec.exe -Wait '/I C:\Packages\MicrosoftEdgeBetaEnterpriseX64.msi /qn' -Verbose 
+    
+}
+InstallEdgeChromium
 # Install .NET Core SDK
 choco install dotnetcore-sdk -confirm:$false
 # Install PowerShell Core 7
